36f31c1108d02458b659ee8b07f6b91bddbcf5c4
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 03:43:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from .auth import routes as auth_routes
+from .ads_routes import router as ads_router  # если ты сохранишь как ads_routes.py
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Сайт работает!"}
+app.include_router(auth_routes.router, prefix="/auth", tags=["auth"])
+app.include_router(ads_router, prefix="/ads", tags=["ads"])
