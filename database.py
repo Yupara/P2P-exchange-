3aba1ea7f1b9d933432102b3ad2be0c776fe436e
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 02:23:56 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./p2p.db"  # или PostgreSQL
+DATABASE_URL = "sqlite:///./p2p.db"  # можешь заменить на PostgreSQL позже
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
