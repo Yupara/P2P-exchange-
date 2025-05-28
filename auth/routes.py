64c85118d38da0f4de0858e4f16ebdc70a114a3f
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 03:09:25 +0500

    Update routes.py

--- auth/routes.py
+++ auth/routes.py
@@ -1,21 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-from ..database import SessionLocal
-from . import schemas, services
-from .. import models
+from fastapi import APIRouter
 
 router = APIRouter()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@router.post("/register", response_model=schemas.UserOut)
-def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
-    if existing_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return services.create_user(db, user.email, user.password)
+@router.get("/test")
+def test():
+    return {"message": "работает"}
