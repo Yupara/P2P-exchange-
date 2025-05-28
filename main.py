4bfa63eac1e5417075271fb40bbfe782cffc345d
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 03:33:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from database import engine
-import models
-import routes
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(routes.router, prefix="/auth", tags=["auth"])
+@app.get("/")
+def root():
+    return {"message": "Сайт работает!"}
