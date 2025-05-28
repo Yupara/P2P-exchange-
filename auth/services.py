6af7668d462b2cdb124d586fdb8594a864f2843e
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 02:22:18 +0500

    Update services.py

--- auth/services.py
+++ auth/services.py
@@ -1,22 +1,13 @@
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
-from .. import models, schemas
+from .. import models
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def create_user(db: Session, user: schemas.UserCreate):
-    hashed_password = get_password_hash(user.password)
-    db_user = models.User(email=user.email, hashed_password=hashed_password)
+def create_user(db: Session, email: str, password: str):
+    hashed_password = pwd_context.hash(password)
+    db_user = models.User(email=email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
-
-def authenticate_user(db: Session, email: str, password: str):
-    user = db.query(models.User).filter(models.User.email == email).first()
-    if not user or not pwd_context.verify(password, user.hashed_password):
-        return None
-    return user
