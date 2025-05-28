03836c4dbb22f534602637a1cbe9d60aea524878
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 02:40:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .auth import routes as auth_routes
+from .auth import routes as auth_routes  # подключаем роуты
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 app.include_router(auth_routes.router, prefix="/auth", tags=["auth"])
