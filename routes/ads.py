d4f1b48d8d29eff37932f79cba5024088ca4b05a
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 14:24:17 +0500

    Update ads.py

--- routes/ads.py
+++ routes/ads.py
@@ -1,5 +1,15 @@
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
-import schemas
 import models
+import schemas
 import database
+
+router = APIRouter()
+
+@router.post("/ads", response_model=schemas.AdOut)
+def create_ad(ad: schemas.AdCreate, db: Session = Depends(database.get_db)):
+    db_ad = models.Advertisement(**ad.dict())
+    db.add(db_ad)
+    db.commit()
+    db.refresh(db_ad)
+    return db_ad
