61e9315fde70300b847e94c05452dda1256a653c
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 15:19:56 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,9 @@
-from sqlalchemy import Column, Integer, String, Float, Enum
+from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
-class Advertisement(Base):
-    __tablename__ = "advertisements"
+class User(Base):
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    crypto = Column(String, index=True)  # BTC, USDT и т.д.
-    fiat = Column(String, index=True)    # USD, RUB и т.д.
-    payment_method = Column(String, index=True)  # Bank Transfer, QIWI и т.п.
-    price = Column(Float)
-    type = Column(String)  # 'buy' или 'sell'
-    available = Column(Float)  # Сколько доступно
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_verified = Column(Boolean, default=False)
