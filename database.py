64293adc2e99d989b057bde0319af90e8676fcf0
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 21:41:33 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"  # или твоя база PostgreSQL на Render
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"  # или URL Render
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
-# ✅ Вот эта функция нужна
 def get_db():
     db = SessionLocal()
     try:
