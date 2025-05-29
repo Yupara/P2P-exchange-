f5bba7aab4da2888393726df3b5de3f16ace8abb
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 22:02:09 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,1 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"  # или URL Render
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+SQLALCHEMY_DATABASE_URI = "postgresql://<user>:<password>@<host>:<port>/<dbname>"
