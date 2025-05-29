3e6df6a01cbb73c33ae54015c4f3d104533faabe
Yupara <parviz.yusupov03@mail.ru>
2025-05-30 01:36:56 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,8 +1,46 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+
+# ----------- Пользователи -----------
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
 
 class UserOut(BaseModel):
     id: int
-    email: str
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
+
+
+# ----------- Токен -----------
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+# ----------- Объявления -----------
+
+class AdBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    price: float
+    currency: str
+    payment_method: Optional[str] = None
+
+
+class AdCreate(AdBase):
+    pass
+
+
+class AdOut(AdBase):
+    id: int
+    owner_id: int
 
     class Config:
         orm_mode = True
