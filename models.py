1103f45b03b22c4261e2585e30033f79f2b72f42
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 15:18:40 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Float, Enum
-from .database import Base
+from database import Base
 
 class Advertisement(Base):
     __tablename__ = "advertisements"
