8b6aad891ad16cda41790fb7c1e95888a3decfb6
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 22:02:53 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,5 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://<user>:<password>@<host>:<port>/<dbname>"
+import os
+from sqlalchemy import create_engine
+
+database_url = os.getenv("DATABASE_URL", "sqlite:///p2p.db")  # Фallback на SQLite
+engine = create_engine(database_url)
