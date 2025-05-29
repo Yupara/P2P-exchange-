21be4e592f3a577c867306229e481b45483cf72e
Yupara <parviz.yusupov03@mail.ru>
2025-05-30 00:15:20 +0500

    Create deps.py

---
+++ auth/deps.py
@@ -0,0 +1,18 @@
+# auth/deps.py
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from database import get_db
+import models
+from auth.jwt_handler import verify_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    email = verify_token(token)
+    if not email:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    user = db.query(models.User).filter(models.User.email == email).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
