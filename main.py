f96b81c659952f72eb5046f53010115017c51e77
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 14:23:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routes import ads as ads_routes  # Импортируем роуты
+from routes import ads as ads_routes  # без точек!
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Подключаем роуты объявлений
 app.include_router(ads_routes.router, prefix="/ads", tags=["ads"])
