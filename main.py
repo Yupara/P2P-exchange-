d70369e6ee1014d18ba56873f83c9e6843283ed3
Yupara <parviz.yusupov03@mail.ru>
2025-05-29 22:54:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routes import ads as ads_routes
+from routes.ads import router as ads_router
+from auth.routes import router as auth_router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(ads_routes, prefix="/ads", tags=["ads"])
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(ads_router, prefix="/ads", tags=["ads"])
