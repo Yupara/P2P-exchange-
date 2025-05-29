64be708e0a1938f072cd127be1635201604027af
Yupara <parviz.yusupov03@mail.ru>
2025-05-30 02:21:10 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,24 @@
-from sqlalchemy import Column, Integer, String
-from database import Base  # Убедись, что путь к Base правильный
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
 
-    # дополнительные поля по желанию:
-    # is_active = Column(Boolean, default=True)
+    ads = relationship("Ad", back_populates="owner")
+
+
+class Ad(Base):
+    __tablename__ = "ads"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    price = Column(Integer)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="ads")
