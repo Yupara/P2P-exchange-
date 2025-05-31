660536a666e459bd4be91f78e65834875eebbfca
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 16:11:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
-from routes import router as ads_router
+from ads_routes import router as ads_router
 from auth.auth import router as auth_router
 from db import models, database
 
-# Создание базы данных
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
-# Подключение маршрутов
 app.include_router(auth_router)
 app.include_router(ads_router)
 
