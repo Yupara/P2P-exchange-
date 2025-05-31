596f6a0b7e4c7c57e5aba06d622ebda53aa7702b
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 16:18:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
-from db import models, database
-from auth.auth import router as auth_router
-from routes.ads import router as ads_router
-
-models.Base.metadata.create_all(bind=database.engine)
+from routes.ads import router as ads_router  # <-- ВАЖНО: ads, а не просто routes
 
 app = FastAPI()
 
-app.include_router(auth_router)
 app.include_router(ads_router)
-
-@app.get("/")
-def root():
-    return {"message": "Welcome to P2P Exchange API"}
