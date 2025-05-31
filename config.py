2d35df388f82df94d1ecec6dd44b8eaf2d6b33f1
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 22:07:31 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
-from datetime import timedelta
-
-# Секретный ключ для подписи JWT (возьмите что-то сложное в реальной жизни)
-SECRET_KEY = "supersecretkey123"
-
-# Алгоритм подписи JWT
+SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
-
-# Срок жизни токена (в минутах)
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
