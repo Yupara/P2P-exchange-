29a6e647f48ddcaa637efbe191834f5fbc3f2aca
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 19:50:41 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,9 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine
-from contextlib import contextmanager
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Замените на вашу строку подключения если используете PostgreSQL
+DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-# 👇 ДОБАВЬТЕ ЭТУ ФУНКЦИЮ
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
