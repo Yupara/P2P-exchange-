5e0470c9fd38ce4de8cd3a12a754c501e9f2d1ad
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 21:41:36 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 
+# Общие схемы для User
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
@@ -8,16 +9,19 @@ class UserCreate(BaseModel):
 class UserOut(BaseModel):
     id: int
     email: EmailStr
-    class Config:
-        from_attributes = True
 
+    class Config:
+        from_attributes = True  # Для Pydantic v2
+
+# Схема токена
 class Token(BaseModel):
     access_token: str
     token_type: str
 
 class TokenData(BaseModel):
-    email: str
+    email: Optional[str] = None
 
+# Общие схемы для Ad
 class AdBase(BaseModel):
     title: str
     description: str
@@ -29,5 +33,6 @@ class AdCreate(AdBase):
 class AdOut(AdBase):
     id: int
     owner_id: int
+
     class Config:
         from_attributes = True
