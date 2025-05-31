071dabdf41c28f7091a4f74e2882ccb8218b5c3a
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 22:21:18 +0500

    Update deps.py

--- auth/deps.py
+++ auth/deps.py
@@ -1,14 +1,15 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
+from auth.jwt_handler import decode_token
 from database import SessionLocal
 from models import User
 from config import SECRET_KEY, ALGORITHM
-from schemas import TokenData
+from fastapi.security import OAuth2PasswordBearer
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
+# Получаем сессию базы данных
 def get_db():
     db = SessionLocal()
     try:
@@ -16,18 +17,22 @@ def get_db():
     finally:
         db.close()
 
+# Получаем текущего пользователя по токену
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
     credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials"
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
+        payload = decode_token(token)
+        user_id: int = payload.get("user_id")
+        if user_id is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
-    user = db.query(User).filter(User.username == username).first()
+
+    user = db.query(User).filter(User.id == user_id).first()
     if user is None:
         raise credentials_exception
     return user
