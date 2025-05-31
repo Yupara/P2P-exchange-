553199d96820d7957d59d40340a2ac1636761d05
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 21:51:53 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,28 +1,4 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional
-
-# --- Схемы для User ---
-
-class UserCreate(BaseModel):
-    email: EmailStr
-    password: str
-
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-
-    class Config:
-        from_attributes = True  # Pydantic v2: «использовать атрибуты модели»
-
-
-# --- Схема токена JWT ---
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-# --- Общие схемы для Ad ---
+from pydantic import BaseModel
 
 class AdBase(BaseModel):
     title: str
