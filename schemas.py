b70b8b26b21536d0b2bc3fd465bb6d9d63bc75fa
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 00:59:30 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,21 +1,8 @@
 from pydantic import BaseModel
 
-class UserCreate(BaseModel):
-    username: str
-    password: str
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-class AdCreate(BaseModel):
-    title: str
-    description: str
-    price: float
-
-class AdOut(AdCreate):
+class UserOut(BaseModel):
     id: int
-    owner_id: int
+    username: str
 
     class Config:
-        from_attributes = True  # Для Pydantic v2
+        from_attributes = True  # заменяет orm_mode в Pydantic v2
