21d5fd6a991aff5d40d97f1b3f9aa6be019adfe7
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 19:49:22 +0500

    Update jwt_handler.py

--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -1,19 +1,11 @@
 from datetime import datetime, timedelta
-from jose import JWTError, jwt
+from jose import jwt
 
-SECRET_KEY = "твой_длинный_секрет_здесь"
+SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24  # токен на сутки
 
-def create_access_token(data: dict):
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=30)):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-def verify_token(token: str):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload.get("sub")
-    except JWTError:
-        return None
