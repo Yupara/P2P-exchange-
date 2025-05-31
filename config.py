6510ce1c8e5e67cee1a146b95920b71259c9ea74
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 02:35:33 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-SECRET_KEY = "your_secret_key"
+# config.py
+
+SECRET_KEY = "your_secret_key_here"          # Замените на что-то надёжное
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
