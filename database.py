4654fec57da2fcd7a9200d62b0de86e5c1fe6717
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 21:48:23 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,12 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Для простоты используем SQLite в файле test.db
+# В данном примере мы используем SQLite (файл test.db).
+# Если вы хотите PostgreSQL или другое, замените строку ниже на вашу URI.
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}  # SQLite-специфика
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
