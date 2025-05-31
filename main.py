1e855b3d061703297467524aff5c7efa9b319e8e
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 02:01:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-# main.py
-
 from fastapi import FastAPI
-from database import Base, engine
-from models import *  # чтобы SQLAlchemy создал таблицы
 from auth.routes import router as auth_router
-from ads_routes import router as ads_router  # или from ads.routes import router as ads_router, если в папке
-
-Base.metadata.create_all(bind=engine)
+from ads_routes import router as ads_router
 
 app = FastAPI(title="P2P Exchange API")
 
+# Регистрируем роуты
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(ads_router, prefix="/ads", tags=["ads"])
 
 @app.get("/", tags=["Root"])
 def root():
-    return {"message": "Welcome to P2P Exchange API"}
+    return {"message": "P2P Exchange API работает"}
