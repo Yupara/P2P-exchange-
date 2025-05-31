b223931cfea08cf14ae6ae010026689d93d86d72
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 21:48:46 +0500

    Update models.py

--- models.py
+++ models.py
@@ -5,14 +5,14 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
 
 class Ad(Base):
     __tablename__ = "ads"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String)
-    price = Column(Float)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    title = Column(String, index=True, nullable=False)
+    description = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
