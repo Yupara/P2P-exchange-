6df89113a00581b6cdfd691446f805dd5ee04090
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 02:12:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-from auth.routes import router as auth_router
-from ads_routes import router as ads_router
 
-app = FastAPI(title="P2P Exchange API")
+app = FastAPI()
 
-# Регистрируем роуты
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(ads_router, prefix="/ads", tags=["ads"])
-
-@app.get("/", tags=["Root"])
+@app.get("/")
 def root():
-    return {"message": "P2P Exchange API работает"}
+    return {"message": "P2P Exchange работает!"}
