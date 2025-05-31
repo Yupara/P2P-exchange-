274c9716d5a172f5405a62171fc71198ba1f8ea8
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 03:13:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 from auth.routes import router as auth_router
-from ads.routes import router as ads_router
+from ads.routes import router as ads_router  # <-- если папка ads и в ней routes.py
 from database import Base, engine
 
-# Создание таблиц
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Подключаем маршруты
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(ads_router, prefix="/ads", tags=["ads"])
