f8cdcb3e4e94c5a1af546cc1c22c2909405a411d
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 23:46:08 +0500

    Update jwt_handler.py

--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -1,18 +1,15 @@
-# auth/jwt_handler.py
-
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
 
-SECRET_KEY = "your_secret_key"
+SECRET_KEY = "mysecret"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 def decode_token(token: str):
     try:
