b1c0fa0fe4b67d53c7376a912572b1605ea1da75
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 02:38:15 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,25 @@
-from sqlalchemy import Column, Integer, String
+# models.py
+
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
     username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    ads = relationship("Ad", back_populates="owner")
+
+
+class Ad(Base):
+    __tablename__ = "ads"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates="ads")
