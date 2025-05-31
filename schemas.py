85fdb5906517695cb435cd617e8a6b06d806c3a3
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 01:55:02 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel
 
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
 class UserOut(BaseModel):
     id: int
     username: str
 
     class Config:
-        from_attributes = True  # заменяет orm_mode в Pydantic v2
+        from_attributes = True  # ✅ FastAPI 2.x совместимость
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
