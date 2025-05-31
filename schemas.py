e47804b06f7755fb6cce5f3669249b5e6b2fe263
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 22:08:46 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,16 +1,37 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Optional
 
-class AdBase(BaseModel):
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+class AdCreate(BaseModel):
     title: str
     description: str
     price: float
 
-class AdCreate(AdBase):
-    pass
-
-class AdOut(AdBase):
+class AdOut(BaseModel):
     id: int
+    title: str
+    description: str
+    price: float
     owner_id: int
 
     class Config:
-        from_attributes = True
+        orm_mode = True
