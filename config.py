cab7232f0b46f0ce3fe41d8e216b746f08e10c2a
Yupara <parviz.yusupov03@mail.ru>
2025-05-31 21:40:24 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-SECRET_KEY = "supersecretkey"
+from datetime import timedelta
+
+SECRET_KEY = "supersecretkey"        # Замените на что-то своё и сложное в реальном проекте
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 30     # Срок жизни токена в минутах
