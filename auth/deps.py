6adeb8e7c68777c44cd1206eee37ba571c211732
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 15:17:16 +0500

    Update deps.py

--- auth/deps.py
+++ auth/deps.py
@@ -1,18 +1,24 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
-
-from auth.jwt_handler import decode_access_token
 from database import get_db
 from models import User
+from auth.jwt_handler import SECRET_KEY, ALGORITHM
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
-    user_id = decode_access_token(token)
-    if user_id is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = int(payload.get("sub"))
+    except (JWTError, ValueError):
+        raise credentials_exception
     user = db.query(User).filter(User.id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+    if user is None:
+        raise credentials_exception
     return user
