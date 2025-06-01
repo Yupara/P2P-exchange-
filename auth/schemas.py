7f5cfcb60685f4b6bb67cda0c5e90dca25d93ed2
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 15:50:14 +0500

    Update schemas.py

--- auth/schemas.py
+++ auth/schemas.py
@@ -1,17 +1,20 @@
+# auth/schemas.py
+
 from pydantic import BaseModel, EmailStr
 
+
 class UserCreate(BaseModel):
-    username: str
     email: EmailStr
     password: str
 
+
 class UserOut(BaseModel):
     id: int
-    username: str
     email: EmailStr
 
     class Config:
-        orm_mode = True
+        from_attributes = True
+
 
 class Token(BaseModel):
     access_token: str
