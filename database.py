964e90417fd08b2c1727e1e4c6be40bfb3203a92
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 15:13:24 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"  # или PostgreSQL URI
+DATABASE_URL = "sqlite:///./test.db"  # Или укажи PostgreSQL URI
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
