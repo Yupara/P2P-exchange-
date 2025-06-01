b0fabb14566814e9588d8585ee432b4093583eda
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 16:02:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from database import Base, engine
+from fastapi.security import OAuth2PasswordBearer
 from auth.routes import router as auth_router
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
-app.include_router(auth_router)
+
+app.include_router(auth_router, prefix="/auth")
