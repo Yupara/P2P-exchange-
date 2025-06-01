c0b8c2bc6e00bb88284e17d517904caa8b514511
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 15:28:35 +0500

    Update routes.py

--- auth/routes.py
+++ auth/routes.py
@@ -1,9 +1,27 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from auth.schemas import UserCreate, UserOut, Token
+from auth.services import create_user, authenticate_user, get_current_user
 from database import SessionLocal
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
-@router.get("/")
-def read_ads():
-    return {"message": "Ads will be here soon"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/register", response_model=UserOut)
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    return create_user(user, db)
+
+@router.post("/login", response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    return authenticate_user(form_data.username, form_data.password, db)
+
+@router.get("/me", response_model=UserOut)
+def get_me(current_user=Depends(get_current_user)):
+    return current_user
