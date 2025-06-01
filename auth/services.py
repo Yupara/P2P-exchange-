fd4aaf165baede5724870a5234c625779c6c1b5a
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 15:16:29 +0500

    Update services.py

--- auth/services.py
+++ auth/services.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
-from passlib.hash import bcrypt
 from models import User
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_user_by_username(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
@@ -9,15 +11,19 @@ def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
 def create_user(db: Session, username: str, email: str, password: str):
-    hashed_password = bcrypt.hash(password)
-    new_user = User(username=username, email=email, password=hashed_password)
-    db.add(new_user)
+    hashed_password = pwd_context.hash(password)
+    user = User(username=username, email=email, hashed_password=hashed_password)
+    db.add(user)
     db.commit()
-    db.refresh(new_user)
-    return new_user
+    db.refresh(user)
+    return user
 
 def authenticate_user(db: Session, username_or_email: str, password: str):
-    user = db.query(User).filter((User.username == username_or_email) | (User.email == username_or_email)).first()
-    if not user or not bcrypt.verify(password, user.password):
-        return None
-    return user
+    user = (
+        db.query(User)
+        .filter((User.username == username_or_email) | (User.email == username_or_email))
+        .first()
+    )
+    if user and pwd_context.verify(password, user.hashed_password):
+        return user
+    return None
