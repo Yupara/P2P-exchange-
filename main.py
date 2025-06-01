403edd135585eb156cc6802bfd7e5704385bf491
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 15:30:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from auth.routes import router as auth_router
+from ads.routes import router as ads_router
+from database import Base, engine
 
-app = FastAPI()
+app = FastAPI(title="P2P Platform")
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(ads_router, prefix="/ads", tags=["ads"])
