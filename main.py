abf435985cfbef67035ff7b50bcc8d714d0c3e65
Yupara <parviz.yusupov03@mail.ru>
2025-06-01 15:10:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from auth.routes import router as auth_router
-from ads.routes import router as ads_router
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(ads_router, prefix="/ads", tags=["ads"])
